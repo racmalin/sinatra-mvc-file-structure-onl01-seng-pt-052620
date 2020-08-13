5e29370feed395bba5509c2aecf2635cf920e083
Kevin C Malin <racmalin@yahoo.com>
2020-08-13 01:25:55 +0000

    Automatically backed up by Learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,13 +1,15 @@
-class Dog 
+class Dog
+  attr_accessor :name, :breed, :age
+  DOGS = []
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
+    DOGS << self
+  end
 
-attr_accessor :name, :breed, :age 
-
-@@all == []
-
-def initialize(name,breed,age)
-  @name == name
-  @breed == breed
-  @age == age
-end
+  def self.all
+    DOGS
+  end
 
 end
